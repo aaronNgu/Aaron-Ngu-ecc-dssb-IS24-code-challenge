f48b43e15f050f32587d2a65a2b83eb25c5b826e
aaron_other <aaron_other@airwork.local>
2023-10-09 22:58:55 -0700

    Added endpoints for POST and PUT

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-import { startingProducts } from './products';
+import { IProduct, startingProducts } from './products';
 import { query, param, validationResult, matchedData, checkSchema } from 'express-validator';
 
 let products = startingProducts;
+let latestId = 39;
 
 // middleware that is specific to this router
 router.use((_req: any, _res: any, next: () => void) => {
@@ -45,13 +46,53 @@ router.get('/product/:productId',
         return res.send(prod);
     })
 
-router.post('/product', (_req: any, res: { send: (arg0: string) => void }) => {
-    res.send('Add product')
+router.post('/product', async (req: any, res: any) => {
+    await checkSchema({
+        productName: { in: ['body'], notEmpty: { bail: true }, isString: { bail: true } },
+        scrumMasterName: { in: ['body'], notEmpty: { bail: true }, isString: { bail: true } },
+        productOwnerName: { in: ['body'], notEmpty: { bail: true }, isString: { bail: true } },
+        Developers: { in: ['body'], isArray: { bail: true, options: { max: 5 } }, },
+        startDate: { in: ['body'], isISO8601: { bail: true } },
+        methodology: { in: ['body'], isIn: { options: [['Agile', 'Waterfall']], bail: true } },
+        location: { in: ['body'], notEmpty: { bail: true }, isString: { bail: true } },
+    }).run(req);
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.status(400).send(JSON.stringify({ errors: result.array() }));
+    }
+    const data = matchedData(req) as IProduct;
+    latestId += 1;
+    const newProduct = { ...data, productId: latestId };
+    products.push(newProduct)
+    res.send(newProduct);
 })
 
-router.put('/product', (_req: any, res: { send: (arg0: string) => void }) => {
-    res.send('Update product')
-})
+router.put('/product/:productId',
+    async (req: any, res: any) => {
+        await checkSchema({
+            productId: { in: ['params'], isNumeric: { bail: true } },
+            productName: { in: ['body'], optional: true, notEmpty: { bail: true }, isString: { bail: true } },
+            scrumMasterName: { in: ['body'], optional: true, notEmpty: { bail: true }, isString: { bail: true } },
+            productOwnerName: { in: ['body'], optional: true, notEmpty: { bail: true }, isString: { bail: true } },
+            Developers: { in: ['body'], optional: true, isArray: { bail: true, options: { max: 5 } }, },
+            startDate: { in: ['body'], optional: true, isISO8601: { bail: true } },
+            methodology: { in: ['body'], optional: true, isIn: { options: [['Agile', 'Waterfall']], bail: true } },
+            location: { in: ['body'], optional: true, notEmpty: { bail: true }, isString: { bail: true } },
+        }).run(req);
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).send(JSON.stringify({ errors: result.array() }));
+        }
+        const productId = req.params.productId;
+        let prod = products.find((product) => product.productId == productId)
+        if (!prod) {
+            return res.status(404).send(`product with ${productId} not found`);
+        }
+        const data = matchedData(req) as IProduct;
+        const updatedProd = { ...prod, ...data };
+        products = products.map((prod) => { return prod.productId == productId ? updatedProd : prod });
+        return res.send(updatedProd);
+    })
 
 router.delete('/product/:productId',
     param('productId').isNumeric(),

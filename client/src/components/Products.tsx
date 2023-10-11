395f7610a13e0d8c46939f1f15164eb7e78c17b7
aaron_other <aaron_other@airwork.local>
2023-10-10 23:48:45 -0700

    Added ProductsTable component

---
+++ client/src/components/Products.tsx
@@ -0,0 +1,48 @@
+import { Sheet, Table, Typography } from "@mui/joy";
+import { useGetProductsQuery } from "../services/api";
+import { IProduct } from "../services/models";
+
+export const ProductsTable = () => {
+    const { data, isLoading } = useGetProductsQuery({});
+    return (
+        <Sheet sx={{ padding: '0px 100px' }}>
+            <Table
+                color="neutral"
+                size="lg"
+                variant="outlined"
+                stickyFooter
+                stickyHeader
+            >
+                <thead>
+                    <tr>
+                        <th style={{ width: '5%' }} hidden>Id</th>
+                        <th>Name</th>
+                        <th>Owner&nbsp;(g)</th>
+                        <th>Developers&nbsp;(g)</th>
+                        <th>Scrum Master&nbsp;(g)</th>
+                        <th>Start date&nbsp;(g)</th>
+                        <th style={{ width: '5%' }}>Methodology&nbsp;(g)</th>
+                        <th style={{ width: '10%' }}>Location&nbsp;(g)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data?.map((prod: IProduct) => <tr key={prod.productId}>
+                        <td hidden><Typography level="body-lg">{prod.productId} </Typography ></td>
+                        <td><Typography level="body-lg">{prod.productName} </Typography ></td>
+                        <td><Typography level="body-lg">{prod.productOwnerName} </Typography ></td>
+                        <td>{prod.Developers.map((dev) => <div key={dev}> <Typography level="body-sm">{dev} </Typography ></div>)}</td>
+                        <td><Typography level="body-lg">{prod.scrumMasterName} </Typography ></td>
+                        <td><Typography level="body-lg">{new Date(prod.startDate.replace(/\s\+\d{2}:\d{2}$/, '')).toLocaleDateString()} </Typography ></td>
+                        <td><Typography level="body-lg">{prod.methodology} </Typography ></td>
+                        <td><Typography level="body-lg">{prod.location} </Typography ></td>
+                    </tr>)}
+                </tbody>
+                <tfoot >
+                    <tr>
+                        <td>Total products: {data?.length}</td>
+                    </tr>
+                </tfoot>
+            </Table>
+        </Sheet >
+    );
+}

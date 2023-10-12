9a8dad49eb49f8d224ddbc4ea30dacb8d6a897ea
aaron_other <aaron_other@airwork.local>
2023-10-11 21:41:45 -0700

    Make row in Product Table editable

--- client/src/components/ProductsTable.tsx
+++ client/src/components/ProductsTable.tsx
@@ -2,8 +2,12 @@ import { Sheet, Table, Typography } from "@mui/joy";
 import { useGetProductsQuery } from "../services/api";
 import { IProduct } from "../services/models";
 
-export const ProductsTable = () => {
-    const { data, isLoading } = useGetProductsQuery({});
+export interface ProductsTableProps {
+    handleClickRow: Function
+}
+
+export const ProductsTable = ({ handleClickRow }: ProductsTableProps) => {
+    const { data } = useGetProductsQuery({});
     return (
         <Sheet sx={{ padding: '0px 100px' }}>
             <Table
@@ -12,30 +16,32 @@ export const ProductsTable = () => {
                 variant="outlined"
                 stickyFooter
                 stickyHeader
+                hoverRow
             >
                 <thead>
                     <tr>
                         <th style={{ width: '5%' }} >Id</th>
                         <th>Name</th>
                         <th>Owner&nbsp;(g)</th>
-                        <th>Developers&nbsp;(g)</th>
+                        <th style={{ width: '10%' }}>Developers&nbsp;(g)</th>
                         <th>Scrum Master&nbsp;(g)</th>
                         <th>Start date&nbsp;(g)</th>
-                        <th style={{ width: '5%' }}>Methodology&nbsp;(g)</th>
-                        <th style={{ width: '10%' }}>Location&nbsp;(g)</th>
+                        <th>Methodology&nbsp;(g)</th>
+                        <th >Location&nbsp;(g)</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {data?.map((prod: IProduct) => <tr key={prod.productId}>
-                        <td><Typography level="body-lg">{prod.productId} </Typography ></td>
-                        <td><Typography level="body-lg">{prod.productName} </Typography ></td>
-                        <td><Typography level="body-lg">{prod.productOwnerName} </Typography ></td>
-                        <td>{prod.Developers.map((dev) => <div key={dev}> <Typography level="body-sm">{dev} </Typography ></div>)}</td>
-                        <td><Typography level="body-lg">{prod.scrumMasterName} </Typography ></td>
-                        <td><Typography level="body-lg">{new Date(prod.startDate).toLocaleDateString()} </Typography ></td>
-                        <td><Typography level="body-lg">{prod.methodology} </Typography ></td>
-                        <td><Typography level="body-lg">{prod.location} </Typography ></td>
-                    </tr>)}
+                    {data?.map((prod: IProduct) => (
+                        <tr key={prod.productId} onClick={() => handleClickRow(prod)} >
+                            <td><Typography level="body-lg">{prod.productId} </Typography ></td>
+                            <td><Typography level="body-lg">{prod.productName} </Typography ></td>
+                            <td><Typography level="body-lg">{prod.productOwnerName} </Typography ></td>
+                            <td>{prod.Developers.map((dev) => <div key={dev}> <Typography level="body-sm">{dev} </Typography ></div>)}</td>
+                            <td><Typography level="body-lg">{prod.scrumMasterName} </Typography ></td>
+                            <td><Typography level="body-lg">{new Date(prod.startDate).toLocaleDateString()} </Typography ></td>
+                            <td><Typography level="body-lg">{prod.methodology} </Typography ></td>
+                            <td><Typography level="body-lg">{prod.location} </Typography ></td>
+                        </tr>))}
                 </tbody>
                 <tfoot >
                     <tr>
